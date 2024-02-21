7bb9f4f2281ba29dd09a51b6f28a1d82d9f0455c
laudavid <laurene.gdavid@gmail.com>
2024-02-21 23:14:54 +0100

    update time series use case + load csv utils

--- pages/timeseries_analysis.py
+++ pages/timeseries_analysis.py
@@ -7,8 +7,8 @@ import numpy as np
 from PIL import Image
 from prophet import Prophet
 from datetime import date
+from utils import load_data_csv
 from sklearn.metrics import root_mean_squared_error
-
 
 
 
@@ -25,13 +25,15 @@ st.info("""Time series forecasting models are AI models built to make accurate p
             These types of models take into account temporal patterns, such as **trends** (long-term movements), **seasonality** (repeating patterns at fixed intervals), and **cyclic patterns** (repeating patterns not necessarily at fixed intervals)""")
             #unsafe_allow_html=True)
 
-st.markdown("The following image shows the difference patterns (trend, seasonality and cyclic) that can be identified by Time Series models.")
+st.markdown(" ")
 image_ts = Image.open('images/ts_patterns.png')
-st.image(image_ts, width=1000)
+_, col, _ = st.columns([0.15,0.7,0.15])
+with col:
+    st.image(image_ts)
 
 st.markdown("    ")
 
-st.markdown("""Here are some useful applications of time series forecasting:
+st.markdown("""Real-life applications of time series forecasting include:
 - **Finance 💰**: Predict stock prices based on historical data to assist investors and traders in making informed decisions.
 - **Energy ⚡**: Forecast energy consumption patterns to optimize resource allocation, plan maintenance, and manage energy grids more efficiently.
 - **Retail 🏬**: Predict future demand for products to optimize inventory levels, reduce holding costs, and improve supply chain efficiency.
@@ -49,138 +51,150 @@ st.markdown("    ")
 
 ###################################### USE CASE #######################################
 
-st.divider()
-st.markdown("### Power Consumption Forecasting ⚡")
-
-#st.markdown("  ")
-st.warning("""In this example, we built a time series forecasting model that predicts the **daily energy consumption of a household** (or *Global Active Power*) using historical data. 
-            The data was gathered from a house in the South of France, between January 2007 and November 2010 (46 months).
-            """)
-
-st.image("images/power.jpg")
-
-st.markdown("    ")
-
-st.markdown("""**Information on the model**:
-- Our model allows **additional data** (other than Global Active Power) to be given to the model to help improve its performance. Here, you can choose between *Global Intensity*, and the *three levels of Sub-metering* to help forecast the overall energy consumption of a household. 
-- The data is split using a **cut-off date** between the historical data and the data we will predict. The historical data will allow the model to learn patterns in the data. The rest will allow us to evaluate the model's performance, by comparing the real and predicted values.
-            """, unsafe_allow_html=True)
-
-
-st.markdown("    ")
-st.markdown("    ")
-
-
 # LOAD DATASET
-data = pd.read_csv(r"data/household/household_power_consumption_clean.csv")
-data_model = data.copy()
+path_timeseries = r"data/household"
+data_model = load_data_csv(path_timeseries,"household_power_consumption_clean.csv")
 data_model.rename({"Date":"ds", "Global_active_power":"y"}, axis=1, inplace=True)
 data_model.dropna(inplace=True)
 data_model["ds"] = pd.to_datetime(data_model["ds"])
 
-col1, col2 = st.columns([0.2,0.8], gap="medium")
+# BEGINNING OF USE CASE
+st.divider()
+st.markdown("### Power Consumption Forecasting ⚡")
 
-with col1:
-    # SELECT CUTOFF DATE
-    all_dates = data_model["ds"].sort_values().unique()
-    #select_cutoff_date = '2010-01-01' # split train/test
+#st.markdown("  ")
+st.info("""In this use case, a time series forecasting model is used to predict the **energy consumption** (or **Global Active Power**) of a household using historical data. 
+        A forecasting model can be a valuable tool to optimize resource planning and avoid overloads during peak demand periods.""")
 
-# Create a date object for "2007-01-01"
-    start_date = date(2009, 5, 1)
-    end_date = date(2010, 9, 1)
+_, col, _ = st.columns([0.15,0.7,0.15])
+with col:
+    st.image("images/energy_consumption.jpg")
 
-    select_cutoff_date = st.slider(
-    "**Select a cut-off date**",
-    min_value=start_date,
-    max_value=end_date,
-    value=date(2010, 1, 1),
-    format="YYYY-MM-DD")
+st.markdown("    ")
+st.markdown("    ")
 
-    #st.write("Start time:", select_cutoff_date )
+st.markdown("#### About the data 📋")
 
-    #select_cutoff_date = st.date_input("**Select a cut-off date**", min_value=start_date, max_value=end_date, value=date(2010, 1, 1)) #datetime.date(2019, 7, 6))
-    select_cutoff_date = select_cutoff_date.strftime('%Y-%m-%d')
+st.markdown("""You were provided the **daily energy consumption** of a household between January 2007 and November 2010 (46 months). 
+            The goal is to forecast the **Global active power** being produced daily by the household. 
+            Additional variables such as **Global Intensity** and three levels of **Sub-metering** are also available for the forecast.
+            """, unsafe_allow_html=True)
 
-    # ADD VARIABLES TO ANALYSIS
-    add_var = ["Sub_metering_1", "Sub_metering_2", "Sub_metering_3","Global_intensity"]
-    st.markdown("")
-    select_add_var = st.multiselect("**Add variables to the model**", add_var)
-    st.markdown("")
-    run_model = st.button("**Run the model**", type="primary")
+st.info("""Forecasting models are **supervised learning models**. 
+        Historical data is used to train the model (same as labeled data). 
+        The other half of the data will be used to assess our model's performance. """)
 
-with col2:
-    # SELECT TRAIN/TEST SET
-    train = data_model[data_model["ds"] <= select_cutoff_date]
-    test = data_model[data_model["ds"] > select_cutoff_date]
 
-    # PLOT TRAIN/TEST SET
-    train_plot = train.copy()
-    train_plot["split"] = ["historical data"]*len(train_plot)
+# SELECT CUTOFF DATE
+all_dates = data_model["ds"].sort_values().unique()
+#select_cutoff_date = '2010-01-01' # split train/test
 
-    test_plot = test.copy()
-    test_plot["split"] = ["to be predicted"]*len(test_plot)
-    data_clean_plot = pd.concat([train_plot, test_plot]) # plot dataset
+# # Create a date object for "2007-01-01"
+# start_date = date(2009, 5, 1)
+# end_date = date(2010, 9, 1)
 
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Global active power", "Sub metering 1", "Sub metering 2", "Sub metering 3", "Global Intensity"])
+# select_cutoff_date = st.slider(
+# "**Select a cut-off date**",
+# min_value=start_date,
+# max_value=end_date,
+# value=date(2010, 1, 1),
+# format="YYYY-MM-DD")
 
-    with tab1:
-        ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
-            x=alt.X('ds:T', title="Date"),
-            y=alt.Y('y:Q', title="Global active power"),
-            color='split:N',
-        ).interactive()
+# #st.write("Start time:", select_cutoff_date )
+# select_cutoff_date = select_cutoff_date.strftime('%Y-%m-%d')
+select_cutoff_date = date(2010, 1, 1)
+select_cutoff_date = select_cutoff_date.strftime('%Y-%m-%d')
 
-        st.markdown("**View Global active power** (to be forecasted)")
-        st.altair_chart(ts_chart, use_container_width=True)
-        st.info("""**Global active power** refers to the total real power consumed by electrical devices in the house (in kilowatts).""")
+# SELECT TRAIN/TEST SET
+train = data_model[data_model["ds"] <= select_cutoff_date]
+test = data_model[data_model["ds"] > select_cutoff_date]
 
+# PLOT TRAIN/TEST SET
+train_plot = train.copy()
+train_plot["split"] = ["historical data"]*len(train_plot)
+
+test_plot = test.copy()
+test_plot["split"] = ["to be predicted"]*len(test_plot)
+data_clean_plot = pd.concat([train_plot, test_plot]) # plot dataset
+
+st.markdown(" ")
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Global active power", "Sub metering 1", "Sub metering 2", "Sub metering 3", "Global Intensity"])
+
+with tab1:
+    st.success("""**Global active power** refers to the total real power consumed by electrical devices in the house (in kilowatts).""")
+    ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
+        x=alt.X('ds:T', title="Date"),
+        y=alt.Y('y:Q', title="Global active power"),
+        color='split:N',
+    ).interactive()
+
+    st.markdown("**View Global active power** (to be forecasted)")
+    st.altair_chart(ts_chart, use_container_width=True)
     
-    with tab2:
-        ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
-                    x=alt.X('ds:T', title="Date"),
-                    y=alt.Y('Sub_metering_1:Q', title="Sub metering 1"),
-                    color=alt.Color('split:N')) #, scale=custom_color_scale))
-                
-        st.markdown("**View Sub-metering 1** (to be added)")
-        st.altair_chart(ts_chart.interactive(), use_container_width=True)
-        st.info("*Sub-metering 1* is the total active power consumed by the kitchen in the house (in kilowatts).")
 
 
-    with tab3:
-        ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
-            x=alt.X('ds:T', title="Date"),
-            y=alt.Y('Sub_metering_2:Q', title="Sub metering 2"),
-            color=alt.Color('split:N')) #, scale=custom_color_scale))
-
-        st.markdown("**View Sub-metering 2** (to be added)")
-        st.altair_chart(ts_chart.interactive(), use_container_width=True)
-        st.info("*Sub-metering 2* is the total active power consumed by the laundry room in the house (in kilowatts).")
-
-    with tab4:        
-        ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
+with tab2:
+    st.success("**Sub-metering 1** is the total active power consumed by the kitchen in the house (in kilowatts).")
+    ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
                 x=alt.X('ds:T', title="Date"),
-                y=alt.Y('Sub_metering_3:Q', title="Sub metering 3"),
-                color=alt.Color('split:N')) #scale=custom_color_scale))
+                y=alt.Y('Sub_metering_1:Q', title="Sub metering 1"),
+                color=alt.Color('split:N')) #, scale=custom_color_scale))
+            
+    st.markdown("**View Sub-metering 1** (to be added)")
+    st.altair_chart(ts_chart.interactive(), use_container_width=True)
 
-        st.markdown("**View Sub-metering 3** (to be added)")
-        st.altair_chart(ts_chart.interactive(), use_container_width=True)
-        st.info("***Sub-metering 3* is the active power consumed by the electric water heater and air conditioner in the household (in kilowatts).")
 
+with tab3:
+    st.success("**Sub-metering 2** is the total active power consumed by the laundry room in the house (in kilowatts).")
+    
+    ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
+        x=alt.X('ds:T', title="Date"),
+        y=alt.Y('Sub_metering_2:Q', title="Sub metering 2"),
+        color=alt.Color('split:N')) #, scale=custom_color_scale))
 
-    with tab5:
-        custom_color_scale = alt.Scale(range=['red', 'lightcoral'])
+    st.markdown("**View Sub-metering 2** (to be added)")
+    st.altair_chart(ts_chart.interactive(), use_container_width=True)
 
-        ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
+with tab4:    
+    st.success("**Sub-metering 3** is the active power consumed by the electric water heater and air conditioner in the household (in kilowatts).")
+    
+    ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
             x=alt.X('ds:T', title="Date"),
-            y=alt.Y('Global_intensity:Q', title="Global active power"),
-            color=alt.Color('split:N')) # scale=custom_color_scale))
+            y=alt.Y('Sub_metering_3:Q', title="Sub metering 3"),
+            color=alt.Color('split:N')) #scale=custom_color_scale))
 
-        st.markdown("**View Global intensity** (to be added)")
-        st.altair_chart(ts_chart.interactive(), use_container_width=True)
-        st.info("**Global intensity** is the average current intensity delivered to the household (amps).")
+    st.markdown("**View Sub-metering 3** (to be added)")
+    st.altair_chart(ts_chart.interactive(), use_container_width=True)
+
+with tab5:
+    st.success("**Global intensity** is the average current intensity delivered to the household (amps).")
+
+    custom_color_scale = alt.Scale(range=['red', 'lightcoral'])
+    ts_chart = alt.Chart(data_clean_plot).mark_line().encode(
+        x=alt.X('ds:T', title="Date"),
+        y=alt.Y('Global_intensity:Q', title="Global active power"),
+        color=alt.Color('split:N')) # scale=custom_color_scale))
+
+    st.markdown("**View Global intensity** (to be added)")
+    st.altair_chart(ts_chart.interactive(), use_container_width=True)
 
 
-# st.markdown(" ")
+st.markdown("#### Train the model ⚙️")
+st.info("""In traditional forecasting models, only the historical values of the variable predicted are used to make future predictions. The forecasting model used does allow **additional data** (other than "Global Active Power") to be used for training. 
+        Using more data can help improve the forecasting models performance.""")
+
+
+# ADD VARIABLES TO ANALYSIS
+add_var = ["Sub_metering_1", "Sub_metering_2", "Sub_metering_3","Global_intensity"]
+st.markdown("")
+select_add_var = st.multiselect("**Add variables to the model**", add_var)
+st.markdown("")
+run_model = st.button("**Run the model**", type="primary")
+
+
+st.markdown("    ")
+st.markdown("    ")
+
 
 
 
@@ -197,7 +211,7 @@ if run_model:
         m.fit(train)
         forecast = m.predict(test)
 
-        st.markdown("##### Results")
+        st.markdown("##### See results")
 
         tab1_result, tab2_result, tab3_result, tab4_result = st.tabs(["True vs Predicted", "Trend", "Weekly seasonality", "Yearly seasonality"])
         with tab1_result:
--- utils.py
+++ utils.py
@@ -10,6 +10,12 @@ def load_data_pickle(path, file):
     return df
 
 @st.cache_data
+def load_data_csv(path, file):
+    "Load data from csv file"
+    df = pd.read_csv(os.path.join(path,file))
+    return df
+
+@st.cache_data
 def load_model_pickle(path, file):
     """Load model from pickle file"""
     path_file = os.path.join(path, file)
